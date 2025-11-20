8704bc19a96c91e6ff90680169a6316769aaf799
minwoo <jmw041615@gmail.com>
2025-11-20 15:01:29 +0900

    depth 진행 중

--- scripts/index.js
+++ scripts/index.js
@@ -14,9 +14,28 @@ var banner = new Swiper('main .banner_container .swiper', {
         el: ".swiper-scrollbar",
       },
 })
-/* -----------------------long배너 닫기--------------------- */
+var special = new Swiper('main .special_order_container .special_order_wrap', {
+  spaceBetween:50,
+  slidesPerView:3,
+   navigation: {
+        nextEl: "main .special_order_container .swiper-button-next",
+        prevEl: "main .special_order_container .swiper-button-prev",
+      },
+})
+/* -----------------------변수모음--------------------- */
+const moreBtn = document.querySelector('#lnb .category > ul > li:first-child');
+const hiddenMenu = document.querySelector('#lnb .cate_menu_hidden');
 const longBanner = document.querySelector('header .long_banner');
 const longBannerClose = document.querySelector('header .long_banner .close_btn');
+/* -----------------------long배너 닫기--------------------- */
 longBannerClose.addEventListener('click',()=>{
-    longBanner.style.display = 'none';
+  longBanner.style.display = 'none';
+})
+/* -----------------------숨어있는 메뉴(depth)--------------------- */
+console.log(moreBtn)
+moreBtn.addEventListener('mouseenter', ()=>{
+  hiddenMenu.style.display = 'flex';
+})
+moreBtn.addEventListener('mouseleave', ()=>{
+  hiddenMenu.style.display = 'none';
 })
